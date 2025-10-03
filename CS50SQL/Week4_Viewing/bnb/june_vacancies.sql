6d114286f6c0defd4762d2b80cd184c780550b69
cad0m <124875722+cad0m@users.noreply.github.com>
2025-10-03 08:14:11 +0100

    Update june_vacancies.sql

--- CS50SQL/Week4_Viewing/bnb/june_vacancies.sql
+++ CS50SQL/Week4_Viewing/bnb/june_vacancies.sql
@@ -1,5 +1,7 @@
 CREATE VIEW "june_vacancies" AS
-SELECT "id", "property_type", "host_name", COUNT("date") AS "days_vacant"
-FROM "available"
-WHERE "date" BETWEEN '2023-06-01' AND '2023-06-30'
-GROUP BY "id";
+SELECT l."id", l."property_type", l."host_name", COUNT(a."date") AS "days_vacant"
+FROM "listings" l
+JOIN "availabilities" a ON a."listing_id" = l."id"
+WHERE a."available" = 'TRUE' AND a."date" BETWEEN '2023-06-01' AND '2023-06-30'
+GROUP BY l."id";
+

cc1de480ed67b6f1f46965c11788b5b7870cbdf3
cad0m <124875722+cad0m@users.noreply.github.com>
2025-10-03 08:13:52 +0100

    Update frequently_reviewed.sql

--- CS50SQL/Week4_Viewing/bnb/frequently_reviewed.sql
+++ CS50SQL/Week4_Viewing/bnb/frequently_reviewed.sql
@@ -1,7 +1,7 @@
 CREATE VIEW "frequently_reviewed" AS
-SELECT "listings"."id", "property_type", "host_name", COUNT("reviews"."id") AS "reviews"
-FROM "listings"
-JOIN "reviews" ON "reviews"."listing_id" = "listings"."id"
-GROUP BY "listings"."id"
-order BY "reviews" DESC, "property_type", "host_name"
+SELECT l."id", l."property_type", l."host_name", COUNT("reviews") AS "reviews"
+FROM "listings" l
+JOIN "reviews" r ON r."listing_id" = l."id"
+GROUP BY "listing_id"
+ORDER BY "reviews" DESC, l."property_type", l."host_name"
 LIMIT 100;
